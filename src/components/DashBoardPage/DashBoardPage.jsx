573268100ca251218f91afc02dc37bea9fcd7c66
Hari <harikuruva2003@gmail.com>
2023-08-01 18:54:26 +0530

    Getting Dash Board Data through API calling

--- src/components/DashBoardPage/DashBoardPage.jsx
+++ src/components/DashBoardPage/DashBoardPage.jsx
@@ -2,16 +2,44 @@ import { DataPageHeader } from "../DataPageHeader/DataPageHeader";
 import { LastTransaction } from "../DashBoardPageLastTransaction/DashBoardPageLastTransaction";
 import { Card } from "../DashBoardTotalAmountCards/DashBoardTotalAmountCards";
 import "./DashBoardPage.css";
+import { useEffect, useState } from "react";
 
 export function DashBoardPage() {
+  const [debitData, setDebitData] = useState("");
+  console.log();
   const creditAndDebitCardsData = [
     { money: "$12,750", text: "Credit", image: "/images/image1.png" },
-    { money: "$5,600", text: "Debit", image: "/images/image2.png" },
+    {
+      money: "$434",
+      text: "Debit",
+      image: "/images/image2.png",
+    },
   ];
 
-  function returnCard() {
-    console.log(creditAndDebitCardsData);
+  useEffect(() => {
+    fetch(
+      "https://bursting-gelding-24.hasura.app/api/rest/credit-debit-totals",
+      {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          "x-hasura-admin-secret":
+            "g08A3qQy00y8yFDq3y6N1ZQnhOPOa4msdie5EtKS1hFStar01JzPKrtKEzYY2BtF",
+          "x-hasura-role": "user",
+          "x-hasura-user-id": "1",
+        },
+      }
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        setDebitData(data);
+      })
+      .catch((err) => {
+        console.log("Error" + err);
+      });
+  }, []);
 
+  function returnCard() {
     return (
       <>
         {creditAndDebitCardsData.map((card, index) => {
