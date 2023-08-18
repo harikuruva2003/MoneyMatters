8763f40a0cd112c4c873f6515c32a7adf306e9bf
Hari <harikuruva2003@gmail.com>
2023-08-18 09:35:08 +0530

    Use Graph QL

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import "./App.css";
 import React, { createContext } from "react";
 import MoneyMattersApp from "./components/MoneyMattersApp/MoneyMattersApp";
 import TransactionsStore from "./stores/TransactionsStore";
+import { Company } from "./graphQL/GraphQLQueries";
+import { loadErrorMessages, loadDevMessages, useQuery } from "@apollo/client";
 
 export const TransactionsStoreContext = createContext(null);
 function App() {
-  let transactionStoreInstance = new TransactionsStore();
+  const { loading, error, data } = useQuery(Company);
 
+  const transactionStoreInstance = new TransactionsStore();
   return (
     <TransactionsStoreContext.Provider value={transactionStoreInstance}>
       <MoneyMattersApp />
